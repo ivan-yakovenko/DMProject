b9668191a512a73ceed6b7c0909eb1fba9c46511
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-04-21 15:12:18 +0300

    added basic implementation of DFS

--- Project/Algorithms/DFS.cs
+++ Project/Algorithms/DFS.cs
@@ -1,6 +1,31 @@
+using Project.Graphs;
+
 namespace Project.Algorithms;
 
-public class DFS
+public static class DFS
 {
-    
+    public static void DFSUse(UnweightedGraph<int> graph, int startVertex)
+    {
+        bool[] visited = new bool[graph.Size];
+        Stack<int> vertices = new Stack<int>();
+
+        visited[startVertex] = true;
+        vertices.Push(startVertex);
+
+        while (vertices.Count != 0)
+        {
+            int vertex = vertices.Pop();
+
+            foreach (int neighbour in graph.AdjacencyList[vertex])
+            {
+                if (!visited[neighbour])
+                {
+                    vertices.Push(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+            
+        }
+    }
+
 }
