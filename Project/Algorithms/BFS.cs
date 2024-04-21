8dac491d5acf61b6579d9edbbfc1394fabac757b
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-04-21 15:31:06 +0300

    added basic implementation of BFS

--- Project/Algorithms/BFS.cs
+++ Project/Algorithms/BFS.cs
@@ -1,6 +1,30 @@
+using Project.Graphs;
+
 namespace Project.Algorithms;
 
 public class BFS
 {
-    
+    public static void BFSUse(UnweightedGraph<int> graph, int startVertex)
+    {
+        bool[] visited = new bool[graph.Size];
+        Queue<int> vertices = new Queue<int>();
+
+        visited[startVertex] = true;
+        vertices.Enqueue(startVertex);
+
+        while (vertices.Count != 0)
+        {
+            int vertex = vertices.Dequeue();
+
+            foreach (int neighbour in graph.AdjacencyList[vertex])
+            {
+                if (!visited[neighbour])
+                {
+                    vertices.Enqueue(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+            
+        }
+    }
 }
